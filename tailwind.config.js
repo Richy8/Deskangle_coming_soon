80b2db39c71593b4c5c8adc98628663463ca5c8b
richy8 <menaelvisjones@gmail.com>
2021-02-25 19:27:57 +0100

    First coming soon page commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "deep-orange": "#F15A23",
+        "deep-purple": "#300034",
+        "deep-green": "#4DE165",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
